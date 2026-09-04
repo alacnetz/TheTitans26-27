277d3e4a65f577edc7654833c552c49255d41dfe
alacnetz <cameron.netz@alaschools.org>
2026-09-04 11:11:06 -0700

    Locations of Servo/Motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/mechanisms/Claw.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode.nextFTC.mechanisms;
 
 import com.pedropathing.ivy.Command;
 
+import dev.nextftc.hardware.RobotController;
 import dev.nextftc.hardware.actuators.NextServo;
 import dev.nextftc.robot.Mechanism;
 
 public class Claw implements Mechanism {
-    NextServo servo = new NextServo("clawServo");
+    NextServo servo = new NextServo(RobotController.controlHub(), 0);
 
     public Command open() {return instant(() -> servo.setPosition(0.2));}
     public Command close() {return instant(() -> servo.setPosition(0.8));}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/mechanisms/Intake.java
@@ -3,12 +3,13 @@ package org.firstinspires.ftc.teamcode.nextFTC.mechanisms;
 
 import com.pedropathing.ivy.Command;
 
+import dev.nextftc.hardware.RobotController;
 import dev.nextftc.hardware.actuators.NextMotor;
 import dev.nextftc.hardware.sensors.NextDistanceSensor;
 import dev.nextftc.robot.Mechanism;
 
 public class Intake implements Mechanism {
-    NextMotor motor = new NextMotor("intakeMotor");
+    NextMotor motor = new NextMotor(RobotController.controlHub(), 0);
     NextDistanceSensor sensor = new NextDistanceSensor("intakeSensor");
 
     @Override
